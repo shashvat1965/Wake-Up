382445586e0cc70ccb7816f21c3fa82b084dff67
shashvat-multipl <shashvat@multipl.xyz>
2023-11-14 22:04:08 +0530

    Initial commit

---
+++ src/home/home.js
@@ -0,0 +1,10 @@
+import {View} from 'react-native'
+import { Heading } from "tamagui";
+
+export const HomePage = ({navigation}) => {
+  return(
+    <View>
+      <Heading>Home Page</Heading>
+    </View>
+  )
+}
